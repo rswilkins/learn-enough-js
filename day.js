933146ea0a7e2bf720280946322e9812edd7ee3e
Ryan Wilkins <rwilkins@rwilkins-ltm.internal.salesforce.com>
2018-05-07 14:21:29 -0400

    add day.js to commit

--- day.js
+++ day.js
@@ -3,3 +3,7 @@ function dayOfTheWeek(date) {
     const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     return daysOfTheWeek[date.getDay()];
 }
+
+function greeting(date) {
+	return `Hello world! Today is ${daysOfTheWeek(date)}`;
+}
